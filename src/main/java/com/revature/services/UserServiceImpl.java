47ff52cee4e9fabccea57fb852633ea3ac65c907
NickUngar1 <76971050+NickUngar1@users.noreply.github.com>
2021-03-17 11:43:51 -0400

    services update

---
+++ src/main/java/com/revature/services/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.beans.User;
+import com.revature.repos.UserRepo;
+
+
+
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	UserRepo ur;
+
+	@Override
+	public User addUser(User u) {
+		return ur.save(u);
+	}
+
+	@Override
+	public User getUser(int id) {
+		return ur.findById(id).get();
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return (List<User>) ur.findAll();
+	}
+
+	@Override
+	public User updateUser(User u) {
+		return ur.save(u);
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		try {
+			ur.delete(ur.findById(id).get());
+			return true;
+		} catch(IllegalArgumentException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	
+	
+
+}
